e0447d2b6e0ec06f8af4aa400ee35828bf5df9d5
faraz343 <64894030+faraz343@users.noreply.github.com>
2026-08-21 20:06:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import smtplib
 import os
 
 # import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+MY_EMAIL = os.environ.get("faraz945@gmail.com")
+MY_PASSWORD = os.environ.get("waagxqcmwqzkyvzd")
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
@@ -28,7 +28,7 @@ if today_tuple in birthdays_dict:
         contents = letter_file.read()
         contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(
